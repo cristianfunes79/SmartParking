fe6200d52d919d31c3e6ed2dfe47a6bd61afb0b0
Cristian Funes <cristianfunes79@gmail.com>
2023-07-08 11:19:02 -0300

    Working example.

--- src/main.c
+++ src/main.c
@@ -16,8 +16,81 @@
 #include <zephyr/net/wifi_mgmt.h>
 #include <zephyr/net/net_event.h>
 
+// TCP
+#include <zephyr/net/net_ip.h>
+#include <zephyr/net/socket.h>
+#include <zephyr/net/websocket.h>
+
 //
-#include "hmc_5883l_driver.h"
+#include "hmc_5883l_driver_port.h"
+
+static bool flagIP = false;
+
+
+/**
+ * 
+ * Socket stuff
+*/
+
+static int setup_socket(sa_family_t family, const char *server, int port,
+			int *sock, struct sockaddr *addr, socklen_t addr_len)
+{
+	const char *family_str = "IPv4";
+	int ret = 0;
+
+	memset(addr, 0, addr_len);
+
+    net_sin(addr)->sin_family = AF_INET;
+    net_sin(addr)->sin_port = htons(port);
+    inet_pton(AF_INET, server, &net_sin(addr)->sin_addr);
+
+    *sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (*sock < 0) {
+		printk("Failed to create %s HTTP socket (%d)", family_str,
+			-errno);
+	}
+    else
+    {
+        printk("Socket successfully created.\n");
+    }
+
+	return ret;
+}
+
+static int connect_socket(sa_family_t family, const char *server, int port,
+			  int *sock, struct sockaddr *addr, socklen_t addr_len)
+{
+	int ret;
+
+	ret = setup_socket(family, server, port, sock, addr, addr_len);
+	if (ret < 0 || *sock < 0) {
+		return -1;
+	}
+
+    printk("Connecting...\n");
+	ret = connect(*sock, addr, addr_len);
+    printk("Done!\n");
+	if (ret < 0) {
+		printk("Cannot connect to %s remote (%d)\n",
+			"IPv4",
+			-errno);
+		ret = -errno;
+	}
+    else
+    {
+        printk("Socket connected.\n");
+    }
+
+	return ret;
+}
+
+static int connect_cb(int sock, struct http_request *req, void *user_data)
+{
+	printk("Websocket %d for %s connected.", sock, (char *)user_data);
+
+	return 0;
+}
 
 
 /**
@@ -53,7 +126,7 @@ static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
     else
     {
         printk("Connected\n");
-        k_sem_give(&wifi_connected);
+        //k_sem_give(&wifi_connected);
     }
 }
 
@@ -64,11 +137,6 @@ static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
     if (status->status)
     {
         printk("Disconnection request (%d)\n", status->status);
-    }
-    else
-    {
-        printk("Disconnected\n");
-        k_sem_take(&wifi_connected, K_NO_WAIT);
     }
 }
 
@@ -98,7 +166,8 @@ static void handle_ipv4_result(struct net_if *iface)
                                 buf, sizeof(buf)));
         }
 
-        k_sem_give(&ipv4_address_obtained);
+        //k_sem_give(&ipv4_address_obtained);
+        flagIP = true;
 }
 
 static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
@@ -107,14 +176,17 @@ static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t
     {
 
         case NET_EVENT_WIFI_CONNECT_RESULT:
+            printk("Received NET_EVENT_WIFI_CONNECT_RESULT.\n");
             handle_wifi_connect_result(cb);
             break;
 
         case NET_EVENT_WIFI_DISCONNECT_RESULT:
+            printk("Received NET_EVENT_WIFI_DISCONNECT_RESULT.\n");
             handle_wifi_disconnect_result(cb);
             break;
 
         case NET_EVENT_IPV4_ADDR_ADD:
+            printk("Received NET_EVENT_IPV4_ADDR_ADD.\n");
             handle_ipv4_result(iface);
             break;
 
@@ -136,9 +208,31 @@ void wifi_connect(void)
     wifi_params.channel = WIFI_CHANNEL_ANY;
     wifi_params.security = WIFI_SECURITY_TYPE_PSK;
     wifi_params.band = WIFI_FREQ_BAND_2_4_GHZ; 
-    wifi_params.mfp = WIFI_MFP_OPTIONAL;
+    wifi_params.mfp = WIFI_MFP_DISABLE;
+    wifi_params.timeout = SYS_FOREVER_MS;
 
     printk("Connecting to SSID: %s\n", wifi_params.ssid);
+
+    if (net_mgmt(NET_REQUEST_WIFI_AP_DISABLE, iface, &wifi_params, sizeof(struct wifi_connect_req_params)))
+    {
+       printk("WiFi in AP Mode Disable Error.\n");
+    }
+    else
+    {
+       printk("WiFi in AP Mode Disable.\n");
+    }
+
+    if (net_mgmt(NET_REQUEST_WIFI_SCAN, iface, &wifi_params, sizeof(struct wifi_connect_req_params)))
+    {
+       printk("WiFi Scan Request Error.\n");
+    }
+    else
+    {
+       printk("WiFi Scan Request Done!.\n");
+    }
+
+    if (net_mgmt_event_wait_on_iface(iface, NET_EVENT_WIFI_SCAN_RESULT, NULL, NULL, NULL, K_FOREVER))
+        printk("Scan done!\n");
 
     if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &wifi_params, sizeof(struct wifi_connect_req_params)))
     {
@@ -146,19 +240,28 @@ void wifi_connect(void)
     }
 	else
 	{
-		printk("WiFi connect request success.");
+		printk("WiFi Connection Request success.\n");
 	}
+
+#if 0
+    if (net_mgmt_event_wait_on_iface(iface, NET_EVENT_WIFI_CONNECT_RESULT, NULL, NULL, NULL, K_FOREVER))
+        printk("WiFi connected.\n");
+#endif
 }
 
 void wifi_status(void)
 {
     struct net_if *iface = net_if_get_default();
-    
+
     struct wifi_iface_status status = {0};
 
     if (net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface, &status,	sizeof(struct wifi_iface_status)))
     {
         printk("WiFi Status Request Failed\n");
+    }
+    else
+    {
+        printk("WiFi Status Request Success.\n");
     }
 
     printk("\n");
@@ -186,21 +289,14 @@ void wifi_disconnect(void)
 static void hmc_thread(void)
 {
 	const struct device *const i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c0));
-	uint8_t hmc_buf[MAX_HMC_FIELDS_];
+    hmc_5883l_driver_t hmc_driver;
+	uint8_t hmc_buf[20];
 	uint8_t ret;
 	uint8_t command[2] = {0x06, 0x03};
 
-    if (hmc_i2cDriverInit(i2c_dev))
-    {
-        printk("I2C device successfully initialized.\r\n");
-    }
-    else
-    {
-        printk("Error during I2C device initialization.\r\n");
-        return;
-    }
-
-	hmc_configureContinuosMode();
+    // Driver initialization
+    hmc_5883l_driverInitPort(&hmc_driver, i2c_dev);
+	hmc_configureContinuosMode(&hmc_driver);
 
 	for(;;)
 	{
@@ -208,26 +304,25 @@ static void hmc_thread(void)
 		uint8_t cmd;
 		k_msleep(100);
 
-
 		cmd=3;
-		ret = i2c_write(i2c_dev, &cmd, 1, HMC5883L_I2C_ADDR);
+		ret = hmc_driver.write(&hmc_driver, &cmd, 1);
 		if (ret)
 			printk("Error on write Mode.");
-		ret = i2c_read(i2c_dev, &hmc_buf[3], 3, HMC5883L_I2C_ADDR);
+		ret = hmc_driver.read(&hmc_driver, &hmc_buf[3], 3);
 		if (ret)
 			printk("Error on write 1st command.\r\n");
 		cmd=6;
-		ret = i2c_write(i2c_dev, &cmd, 1, HMC5883L_I2C_ADDR);
+		ret = hmc_driver.write(&hmc_driver, &cmd, 1);
 		if (ret)
 			printk("Error on write Mode.");
-		ret = i2c_read(i2c_dev, &hmc_buf[6], 3, HMC5883L_I2C_ADDR);
+		ret = hmc_driver.read(&hmc_driver, &hmc_buf[6], 3);
 		if (ret)
 			printk("Error on write 1st command.\r\n");
 		cmd=9;
-		ret = i2c_write(i2c_dev, &cmd, 1, HMC5883L_I2C_ADDR);
+		ret = hmc_driver.write(&hmc_driver, &cmd, 1);
 		if (ret)
 			printk("Error on write Mode.");
-		ret = i2c_read(i2c_dev, &hmc_buf[9], 4, HMC5883L_I2C_ADDR);
+		ret = hmc_driver.read(&hmc_driver, &hmc_buf[9], 4);
 		if (ret)
 			printk("Error on write 1st command.\r\n");
 
@@ -241,13 +336,13 @@ static void hmc_thread(void)
 
 #if 0
 	hmc_buf[0] = 9;
-	ret = i2c_write(i2c_dev, hmc_buf, 1, HMC5883L_I2C_ADDR);
+	ret = hmc_driver.write(&hmc_driver, hmc_buf, 1);
 	if (ret)
 	{
 		printk("Error on 1st write.");
 	}
 
-	ret = i2c_read(i2c_dev, hmc_buf, 4, HMC5883L_I2C_ADDR);
+	ret = hmc_driver.read(&hmc_driver, hmc_buf, 4);
 	if (ret)
 	{
 		printk("Error on 1st write.");
@@ -261,6 +356,43 @@ static void hmc_thread(void)
 		}
 	}
 #endif
+}
+
+static struct net_mgmt_event_callback mgmt_cb;
+
+static void handler(struct net_mgmt_event_callback *cb,
+		    uint32_t mgmt_event,
+		    struct net_if *iface)
+{
+	int i = 0;
+
+	if (mgmt_event != NET_EVENT_IPV4_ADDR_ADD) {
+		return;
+	}
+
+	for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+		char buf[NET_IPV4_ADDR_LEN];
+
+		if (iface->config.ip.ipv4->unicast[i].addr_type !=
+							NET_ADDR_DHCP) {
+			continue;
+		}
+
+		printk("Your address: %s",
+			net_addr_ntop(AF_INET,
+			    &iface->config.ip.ipv4->unicast[i].address.in_addr,
+						  buf, sizeof(buf)));
+		printk("Lease time: %u seconds",
+			 iface->config.dhcpv4.lease_time);
+		printk("Subnet: %s",
+			net_addr_ntop(AF_INET,
+				       &iface->config.ip.ipv4->netmask,
+				       buf, sizeof(buf)));
+		printk("Router: %s",
+			net_addr_ntop(AF_INET,
+						 &iface->config.ip.ipv4->gw,
+						 buf, sizeof(buf)));
+	}
 }
 
 
@@ -277,12 +409,18 @@ void main(void)
 
     net_mgmt_add_event_callback(&wifi_cb);
     net_mgmt_add_event_callback(&ipv4_cb);
+    printk("Callbacks set.\n");
 
+    k_sleep(K_SECONDS(2)); 
     wifi_connect();
-    k_sem_take(&wifi_connected, K_FOREVER);
-    k_sem_take(&ipv4_address_obtained, K_FOREVER);
+    printk("Connection Done...\n");
+    //k_sem_take(&wifi_connected, K_FOREVER);
+    printk("2Connection Done...\n");
+    //k_sem_take(&ipv4_address_obtained, K_FOREVER);
     printk("Ready...\n\n");
-    
+
+    while(!flagIP) k_sleep(K_SECONDS(1));
+
 	#if 0
     printk("Looking up IP addresses:\n");
     struct zsock_addrinfo *res;
@@ -293,19 +431,59 @@ void main(void)
     sock = connect_socket(&res);
     http_get(sock, "iot.beyondlogic.org", "/test.txt");
     zsock_close(sock);
-	#endif
     
+    wifi_connect();
     // Stay connected for 30 seconds, then disconnect.
-    //wifi_disconnect();
+	#endif
+
+    while(1)
+    {
+        // Socket stuff
+
+        int sock4 = -1;
+        int websock4 = -1;
+        struct sockaddr_in addr4;
+        size_t amount;
+        int ret;
+
+        (void)connect_socket(AF_INET, "169.55.61.243", 9012,
+                        &sock4, (struct sockaddr *)&addr4,
+                        sizeof(addr4));
+        if (sock4 < 0) {
+            printk("Cannot create HTTP connection.");
+        }
+        else
+        {
+            printk("Connection created.\n");
+
+            printk("Sending...\n");
+
+            char data[] = "ESP32/1.0|POST|BBFF-DQaxQtnjWZzKDzTsVRtOUfNLulXxqf|64a2425cbe387b5d626506e1:pythonTestDevice=>fieldX:20.2|end";
+
+            int rc = send(sock4, data, strlen(data), 0);
+
+            printk("Data sent!\n");
+            
+            if (rc < 0)
+                printk("Error sending.\n");
+            
+            close(sock4);
+        }
+
+        k_sleep(K_MINUTES(10));
+    }
+#if 0
+    wifi_disconnect();
     while(1)
     {
         printk("Looping...\r\n");
-        k_sleep(K_SECONDS(10)); 
+        k_sleep(K_SECONDS(5)); 
         wifi_status();
     }
+    #endif
 }
 
-#if 1
+#if 0
 K_THREAD_DEFINE(hmc_id, STACKSIZE, hmc_thread, NULL, NULL, NULL,
 		PRIORITY, 0, 0);
         #endif
